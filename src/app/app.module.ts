cafd574ba75038e7a461af706ec67b5707c5e94e
Nitin Bhojwani <nbhojwani@vmware.com>
2019-08-28 22:30:19 +0530

    move all the app.component code to separate module weather-status

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,11 @@ import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { WeatherCardComponent } from './weather-card/weather-card.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeatherCardComponent
   ],
   imports: [
     BrowserModule,
